e5565cfafc82086a04312e7b04536f8a7387f254
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2024-01-13 18:15:44 -0300

    estilo

--- index.php
+++ index.php
@@ -6,36 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de aço para orçamento</title>
     <link rel="stylesheet" href="assets/main.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        input {
-            margin-bottom: 10px;
-        }
-
-        button {
-            padding: 5px 10px;
-            font-size: 14px;
-            cursor: pointer;
-        }
-
-        #resultado {
-            margin-top: 20px;
-        }
-    </style>
     <script src="assets/function.js"></script>
 </head>
 
 <body>
-    <header></header>
+    <header>
+        <img src="assets/logo-app.png" alt="Logo">
+    </header>
 
     <label for="largura">Largura (mm):</label>
     <input type="number" id="largura" step="1" required>
@@ -51,7 +28,18 @@
 
     <button onclick="calcularPeso()">Calcular Peso</button>
 
-    <div id="resultado"></div>
+    <div class="resultado">
+        <p class="titulo-resultado" >Resultado</p>
+        <div id="resultado">Faça o cálculo primeiro!</div>
+    </div>
+
+    <h2 class="titulo">Cálculo</h2>
+    <p>Essa calculadora se baseia no <strong>Aço</strong> que tem uma densidade de <strong>7.85 gr/cm3</strong>. Peso = Área × Espessura × Densidade do Aço</p>
+
+    <h2 class="titulo">Sobre o projeto</h2>
+    <p>Esse projeto foi feito pelo <strong>Rangel Amaro</strong>, com intuito de agilizar orçamentos de chapas.</p>
+    <p>Para maior compatibilidade ele foi feita para web. E o melhor de tudo <strong>ESTÁ LIVRE DE ANÚNCIOS CHATOS</strong>.</p>
+    <span class="meu-email">Rangelamaro90@gmail.com</span>
 
 </body>
 
